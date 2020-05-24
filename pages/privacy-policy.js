cb08f452009d6a2a5c44bd9bfc7554b78f5baf57
Andrew Jones <aej11@hotmail.com>
2020-05-24 19:59:05 -0400

    added privacy policy

---
+++ pages/privacy-policy.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <div className="container">
+            <Head>
+                <title>A-Zone Apps & Games</title>
+            </Head>
+
+            <main>
+                <h1 className="title">
+                    A-Zone Apps & Games Privacy Policy
+                </h1>
+
+                <p>A-Zone Apps & Games ("the Company") will not use any of your personal information, including contact info, pictures, etc, unless explicitly stated otherwise.</p>
+                <p>The Company may access your usage information anonymously - this limited information will never be tied to your identity.</p>
+
+            </main>
+        </div>
+    )
+}
